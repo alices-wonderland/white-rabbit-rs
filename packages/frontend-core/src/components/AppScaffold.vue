39ad9d3f6d4d8cbb8d75ad428d1ca6ae201a4bf6
Ukonn Ra <ukonnra@outlook.com>
2023-10-23 18:55:04 +0800

    chore: upgrade yarn 4.0

--- packages/frontend-core/src/components/AppScaffold.vue
+++ packages/frontend-core/src/components/AppScaffold.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout>
-    <q-header reveal>
+  <q-layout view="lHh lpr lff">
+    <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>Header {{ t("test") }} {{ quasar.lang.label.close }}</q-toolbar-title>
         <q-btn flat round dense :icon="darkIcon" @click="toggleDark" />
@@ -19,18 +19,34 @@
           </q-menu>
         </q-btn>
       </q-toolbar>
+
+      <q-toolbar inset>
+        <q-breadcrumbs active-color="white">
+          <q-breadcrumbs-el label="Home" icon="home" />
+          <q-breadcrumbs-el label="Components" icon="widgets" />
+          <q-breadcrumbs-el label="Toolbar" />
+        </q-breadcrumbs>
+      </q-toolbar>
     </q-header>
 
+    <q-footer>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
     <q-page-container>
-      <q-btn color="primary" label="Primary"></q-btn>
-      <q-btn color="secondary" label="Secondary" />
-      <q-btn color="accent" label="Accent" />
-      <q-btn color="dark" label="Dark" />
-      <q-btn color="positive" label="Positive" />
-      <q-btn color="negative" label="Negative" />
-      <q-btn color="info" label="Info" />
-      <q-btn color="warning" label="Warning" />
-      <slot></slot>
+      <q-page padding>
+        <q-btn color="primary" label="Primary"></q-btn>
+        <q-btn color="secondary" label="Secondary" />
+        <q-btn color="accent" label="Accent" />
+        <q-btn color="dark" label="Dark" />
+        <q-btn color="positive" label="Positive" />
+        <q-btn color="negative" label="Negative" />
+        <q-btn color="info" label="Info" />
+        <q-btn color="warning" label="Warning" />
+        <slot></slot>
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
