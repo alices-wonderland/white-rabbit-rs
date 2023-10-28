2be3cb31d76b75f134bd7d4a92b2c7b6c47a3157
Ukonn Ra <ukonnra@outlook.com>
2023-10-28 10:40:19 +0800

    feat: add account commands for table

--- packages/frontend-core/src/components/AppTable/AppTableEditableCellRenderer.vue
+++ packages/frontend-core/src/components/AppTable/AppTableEditableCellRenderer.vue
@@ -1,53 +1,32 @@
 <script setup lang="ts" generic="R">
 import type { ICellRendererParams } from "@ag-grid-community/core";
-import type { EditableValue } from "./index";
+import type { CellState } from "./index";
 import { computed } from "vue";
 
 const props = defineProps<{
-  readonly params: ICellRendererParams<R> & { readonly editableValue?: EditableValue<string> };
+  readonly params: ICellRendererParams<R> & { readonly cellState?: CellState<string> };
 }>();
 
-const value = computed(() => props.params.editableValue);
+const state = computed(() => props.params.cellState);
 </script>
 
 <template>
-  <div v-if="value" class="flex gap-1 w-full h-full items-center overflow-hidden">
-    <template v-if="value.state === 'NORMAL'">
-      <slot v-if="$slots['default']" :value="value.value"></slot>
-      <span v-else>{{ value.value }}</span>
-    </template>
-    <template v-else-if="value.state === 'NEW'">
-      <q-icon name="new_releases"></q-icon>
-      <slot v-if="$slots['default']" :value="value.value"></slot>
-      <span v-else>{{ value.value }}</span>
-    </template>
-    <template v-else-if="value.state === 'UPDATED'">
-      <slot v-if="$slots['existing']" name="existing" :value="value.existing"></slot>
-      <q-badge v-else color="warning" :label="value.existing">
-        <q-tooltip>
-          <span>
-            <strong>Existing:</strong>
-            <span>{{ value.existing }}</span>
-          </span>
-        </q-tooltip>
-      </q-badge>
-      <q-icon name="arrow_right"></q-icon>
-      <slot v-if="$slots['current']" name="current" :value="value.value"></slot>
-      <q-badge v-else color="positive" :label="value.value">
-        <q-tooltip>
-          <span>
-            <strong>Current:</strong>
-            <span>{{ value.value }}</span>
-          </span>
-        </q-tooltip>
-      </q-badge>
-    </template>
+  <div v-if="state" class="flex gap-1 w-full h-full items-center overflow-hidden">
+    <span>{{ state.value }}</span>
     <q-badge
-      v-if="value.state !== 'NORMAL'"
+      v-if="state.state === 'UPDATED'"
       class="absolute top-0.5 right-0.5"
       align="top"
       rounded
       color="positive"
-    ></q-badge>
+    >
+      <q-tooltip>
+        <slot v-if="$slots['tooltip']" name="tooltip"></slot>
+        <template v-else>
+          <div class="font-bold">Existing Value:</div>
+          <div>{{ state.existing }}</div>
+        </template>
+      </q-tooltip>
+    </q-badge>
   </div>
 </template>
--- packages/frontend-core/src/components/AppTable/index.ts
+++ packages/frontend-core/src/components/AppTable/index.ts
@@ -1,6 +1,67 @@
 export { default as AppTable } from "./AppTable.vue";
 export { default as AppTableEditableCellRenderer } from "./AppTableEditableCellRenderer.vue";
 
-export type EditableValue<V> =
-  | { readonly state: "NEW" | "NORMAL"; readonly value: V }
+export type CellState<V = unknown> =
+  | { readonly state: "NORMAL"; readonly value: V }
   | { readonly state: "UPDATED"; readonly value: V; readonly existing: V };
+
+export type RowState<F extends string = string> =
+  | { readonly state: "NEW" | "DELETED" | "NORMAL" }
+  | { readonly state: "UPDATED"; readonly updatedFields: F[] };
+
+export abstract class AbstractRow<M, F extends string = string> {
+  readonly id: string;
+  protected readonly _existing?: M;
+
+  protected _deleted: boolean = false;
+
+  protected constructor(id: string, existing?: M) {
+    this.id = id;
+    this._existing = existing;
+  }
+
+  abstract get updatableFields(): readonly F[];
+
+  abstract reset(): void;
+
+  abstract getCellState<V>(field: F): CellState<V>;
+
+  get rowState(): RowState {
+    if (!this._existing) {
+      return {
+        state: "NEW",
+      };
+    } else if (this._deleted) {
+      return {
+        state: "DELETED",
+      };
+    }
+
+    const updatedFields = this.updatableFields
+      .map<[F, CellState]>((field) => [field, this.getCellState(field)])
+      .filter(([_field, state]) => state.state === "UPDATED")
+      .map(([field]): F => field);
+
+    if (updatedFields.length > 0) {
+      return {
+        state: "UPDATED",
+        updatedFields: updatedFields,
+      };
+    } else {
+      return {
+        state: "NORMAL",
+      };
+    }
+  }
+
+  get deleted() {
+    return this._deleted;
+  }
+
+  set deleted(value: boolean) {
+    this._deleted = value;
+    if (this._deleted) {
+      this.reset();
+    }
+  }
+}
