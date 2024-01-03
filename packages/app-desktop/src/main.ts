9551efb3b261d93056e5cc4f32722b06e0197dea
Ukonn Ra <ukonnra@outlook.com>
2024-01-03 17:29:39 +0800

    feat: add actions for entry table

--- packages/app-desktop/src/main.ts
+++ packages/app-desktop/src/main.ts
@@ -2,8 +2,13 @@ import "@core/style.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { agGrid, i18n, quasar } from "@core/plugins";
-import { ACCOUNT_API_KEY, JOURNAL_API_KEY, ENTRY_API_KEY } from "@core/services";
-import { accountApi, journalApi, entryApi } from "@/services";
+import {
+  ACCOUNT_API_KEY,
+  JOURNAL_API_KEY,
+  ENTRY_API_KEY,
+  HIERARCHY_REPORT_API_KEY,
+} from "@core/services";
+import { accountApi, journalApi, entryApi, hierarchyReportApi } from "@/services";
 import router from "@core/router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 
@@ -18,4 +23,5 @@ app.use(VueQueryPlugin);
 app.provide(JOURNAL_API_KEY, journalApi);
 app.provide(ACCOUNT_API_KEY, accountApi);
 app.provide(ENTRY_API_KEY, entryApi);
+app.provide(HIERARCHY_REPORT_API_KEY, hierarchyReportApi);
 app.mount("#app");
