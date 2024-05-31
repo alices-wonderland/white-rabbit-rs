ec9ce89f0941c2dbd8f230011c0e3c28da63b586
Ukonn Ra <ukonnra@outlook.com>
2024-05-31 21:39:04 +0800

    feat: remove tauri

--- packages/app-tauri/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import configs from "../../eslint.config.base.mjs";
-
-export default [
-  ...configs,
-  {
-    ignores: ["./src-tauri/gen/schemas"],
-  },
-];
--- packages/app-tauri/postcss.config.mjs
+++
@@ -1,3 +0,0 @@
-import config from "../../postcss.config.mjs";
-
-export default config;
